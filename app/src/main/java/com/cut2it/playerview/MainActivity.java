4205d76777c46ec1db07671dd2f9602565713dc1
Nafeesa <naffu.aziz@gmail.com>
2019-06-24 18:41:33 +0530

    create project for cut2it playerview

---
+++ app/src/main/java/com/cut2it/playerview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cut2it.playerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ playerviewlibrary01/src/main/java/com/cut2it/playerviewlibrary01/Cut2itPlayerView.java
@@ -0,0 +1,20 @@
+package com.cut2it.playerviewlibrary01;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class Cut2itPlayerView extends PlayerView {
+    public Cut2itPlayerView(Context context) {
+        super(context);
+    }
+
+    public Cut2itPlayerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Cut2itPlayerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
