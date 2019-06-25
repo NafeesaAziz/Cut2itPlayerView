80d4a707a179561dea2fc7fcb3a6902bc3efa881
Nafeesa <naffu.aziz@gmail.com>
2019-06-25 10:48:55 +0530

    avcommunication implementation in library.

--- app/src/main/java/com/cut2it/playerview/MainActivity.java
+++ app/src/main/java/com/cut2it/playerview/MainActivity.java
@@ -3,8 +3,11 @@ package com.cut2it.playerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cut2it.playerviewlibrary01.Cut2itPlayerView;
+
 public class MainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ playerviewlibrary01/src/main/java/com/cut2it/playerviewlibrary01/avcommunication/CustomView.java
@@ -0,0 +1,17 @@
+package com.cut2it.playerviewlibrary01.avcommunication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.google.android.exoplayer2.ui.PlayerView;
+
+
+public class CustomView extends PlayerView
+{
+    public CustomView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+
+}
